cdde51291b68d4895030986765de3e1b1b47b750
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-24 11:49:16 +0000

    Update slice.js

--- src/redux/user/slice.js
+++ src/redux/user/slice.js
@@ -1,20 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  user: {
-    id: 1,
-    name: "Matheus Fraga",
-    email: "teste@teste.com",
-    address: null,
-  }
+  user: null,
 }
 
 export const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {
-     
+  name: "user",
+  initialState,
+  reducers: {
+    createUser: (state, action) => {
+
+        if(action.payload.name.lenght <= 4){
+            alert("O NOME DEVE CONTER MAIS DE 4 CARACTERES!")
+            return {...state}
+        }
+        
+      return {
+        ...state,
+        user:{
+          name: action.payload.name,
+          email: action.payload.email,
+          address: null,
+        }
       }
-});
+    },
+    logoutUser: (state) => {
+
+      return {
+        ...state,
+        user: null,
+      }
+    }
+  }
+})
+
+export const { createUser, logoutUser } = userSlice.actions;
 
 export default userSlice.reducer;
+
+
